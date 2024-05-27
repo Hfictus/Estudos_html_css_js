cd49f17cee67e02e8bd0c8b6ebcc812971f0b4e8
Gerson Klauck <gk120hf@gmail.com>
2024-05-27 20:01:42 -0300

    header menu

--- javaScript/functions.js
+++ javaScript/functions.js
@@ -1,12 +1,15 @@
+
+let clicarMenu = document.getElementById('menu-glass')
+clicarMenu.addEventListener("click", clickMenu)
+
 function clickMenu() {
     let menu = document.getElementById('items-menu-glass')
-    let imgGlass = document.getElementById('menu-glass')
     
     if(menu.style.display == 'block') {
         menu.style.display = 'none'
-        imgGlass.style.opacity = .3
+        clicarMenu.style.opacity = .3
     }else{
         menu.style.display = 'block'
-        imgGlass.style.opacity = 1
+        clicarMenu.style.opacity = 1
     }
 }
