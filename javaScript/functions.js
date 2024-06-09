4d055c59cc70e9e3b80339761fd26ebb55a36138
Gerson Klauck <gk120hf@gmail.com>
2024-06-09 19:56:16 -0300

    header onload

--- javaScript/functions.js
+++ javaScript/functions.js
@@ -1,4 +1,5 @@
 
+//Menu mobile
 let clicarMenu = document.getElementById('menu-glass')
 clicarMenu.addEventListener("click", clickMenu)
 
@@ -12,4 +13,13 @@ function clickMenu() {
         menu.style.display = 'block'
         clicarMenu.style.opacity = 1
     }
+}
+
+function mudaFoto(foto) {
+    document.getElementById("#img-page").src = "img/" + foto + ".webp";
+}
+
+function carregarImg(img) {
+    let img_logo = document.getElementById("img-page");
+    img_logo.src = "img/" + img + ".webp";
 }
