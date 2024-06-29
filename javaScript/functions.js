4877ff29e3fe36dbabc6eadf634a4fe5af576cee
Gerson Klauck <gk120hf@gmail.com>
2024-06-29 19:59:44 -0300

    header

--- javaScript/functions.js
+++ javaScript/functions.js
@@ -15,4 +15,19 @@ function clickMenu() {
     }
 }
 
-        
+//Função para verificar a largura da tela e alterar a imagem
+const updateImage = () => {
+    const screenWidth = window.innerWidth;
+    const imgGlass = document.getElementById("img-glass");
+    if(screenWidth <= 600) {
+        imgGlass.src = "img/glass-oculos-preto-peq.webp";
+    }else {
+        imgGlass.src = "img/glass-oculos-preto-peq2.webp";
+    }
+}       
+
+// Verifique a largura da tela ao carregar a página
+updateImage();
+
+//Adicione um ouvinte de evento para verificar a largura da tela quando a janela for redimensionada
+window.addEventListener("resize", updateImage);
